07fc7358943a7c67753ad93d645f7cca48e45c64
Nitin Patil <Nitin_Patil@epam.com>
2019-02-06 00:53:05 +0530

    My first commit

---
+++ src/test/java/com/myautomation/myAutomationProject/AutomationTest.java
@@ -0,0 +1,47 @@
+package com.myautomation.myAutomationProject;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+
+public class AutomationTest {
+
+	WebDriver driver;
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.setProperty("webdriver.chrome.driver", "browsers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().fullscreen();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
+
+	@Test
+	public void test1() {
+		driver.navigate().to("http://www.google.com");
+		System.out.println(driver.getTitle() + " of test1 method");
+	}
+
+	@Test
+	public void test2() {
+		driver.navigate().to("http://www.google.com");
+		System.out.println(driver.getTitle() + " of test2 method");
+	}
+
+	@Test
+	public void test3() {
+		driver.navigate().to("http://www.google.com");
+		System.out.println(driver.getTitle() + " of test3 method");
+	}
+
+}
