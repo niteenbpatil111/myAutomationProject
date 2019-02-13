c3e242339f3dab0644f23c868d587719cdbd663f
Nitin Patil <Nitin_Patil@epam.com>
2019-02-13 13:52:44 +0530

    updated file

--- src/test/java/com/myautomation/myAutomationProject/AutomationTest.java
+++ src/test/java/com/myautomation/myAutomationProject/AutomationTest.java
@@ -24,6 +24,12 @@ public class AutomationTest {
 		driver = new ChromeDriver();
 		driver.manage().window().fullscreen();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println("beforeMethod executed");
+		System.out.println("beforeMethod finished");
+		System.out.println("Text 1");
+		System.out.println("Text 2");
+
+
 	}
 
 	@AfterMethod
